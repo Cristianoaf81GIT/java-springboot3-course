43d737b6bfbcd6b0377b33e4b218c1f076985eb1
Cristiano Alexandre de Faria <cristianoaf81@gmail.com>
2024-11-05 12:59:07 -0300

    feat(124): customizing swagger docs
    Closes@session15

--- src/main/java/br/com/cristianoaf81/controllers/PersonController.java
+++ src/main/java/br/com/cristianoaf81/controllers/PersonController.java
@@ -16,6 +16,10 @@ import br.com.cristianoaf81.data.vo.v2.PersonVO2;
 import br.com.cristianoaf81.services.PersonServices;
 import br.com.cristianoaf81.utils.MediaType;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -34,7 +38,17 @@ public class PersonController {
 
   @GetMapping(
       produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
-  @Operation(summary = "Finds all people")
+  @Operation(summary = "Finds all people", description = "Finds all people", tags = {"People"},
+      responses = {
+          @ApiResponse(description = "Success", responseCode = "200",
+              content = {@Content(mediaType = "application/json",
+                  array = @ArraySchema(schema = @Schema(implementation = PersonVO.class)))}),
+          @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+          @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+          @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+          @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+
+      })
   public List<PersonVO> findAll() {
     return this.service.findAll();
   }
