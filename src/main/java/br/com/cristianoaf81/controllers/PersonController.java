5ae38b551782cb0b9f67025ec509cac8b31b4448
Cristiano Alexandre de Faria <cristianoaf81@gmail.com>
2024-11-08 14:35:05 -0300

    feat(124): customizing swagger docs
    Closes@session15

--- src/main/java/br/com/cristianoaf81/controllers/PersonController.java
+++ src/main/java/br/com/cristianoaf81/controllers/PersonController.java
@@ -30,8 +30,19 @@ public class PersonController {
   @Autowired
   private PersonServices service;
 
+
   @GetMapping(value = "/{id}",
       produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+  @Operation(summary = "Finds a person", description = "Finds a person", tags = {"People"},
+      responses = {
+          @ApiResponse(description = "Sucess", responseCode = "200",
+              content = @Content(schema = @Schema(implementation = PersonVO.class),
+                  mediaType = "application/json")),
+          @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+          @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+          @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+          @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+          @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),})
   public PersonVO findById(@PathVariable(value = "id") Long id) {
     return this.service.findById(id);
   }
